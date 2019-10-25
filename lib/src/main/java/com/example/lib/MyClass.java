9c6d158765bcc669070ba2abfc648b54f6618059
Mitsuku <kjfgkfdkc@gmail.com>
2019-10-25 17:14:28 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String[] args)
+    {
+        System.out.println("hellow world");
+        System.out.println("3A513246");
+    }
+
+}
